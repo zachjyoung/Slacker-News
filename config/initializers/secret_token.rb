d595940526d63af8320061461f32c81832c8d6cd
Zach Young <zachjyoung@gmail.com>
2013-08-15 14:38:35 -0400

    inital commit, installed rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SlackerNews::Application.config.secret_key_base = 'bf5721963f140fdcacdc173f0b7da43c1aad659df4df7eff5f144394d088d3bc0a9f6c464b1119a29f4c80e9b817ea8eb87459f615edd2ca7110a0fd0d0d088e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+SlackerNews::Application.config.session_store :cookie_store, key: '_slacker_news_session'
